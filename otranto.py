d29e2e05dc0ef8705609260766e1a235e03573dd
Charles Horn <charles.horn@gmail.com>
2019-11-12 21:05:58 +1300

    Lexical Number System class: Lexnum

--- otranto.py
+++ otranto.py
@@ -10,40 +10,35 @@ DOUBLE = {
         'op': lambda x: x * 2
         }
 
+class Lexnum():
+    def __init__(self, s):
+        words = set(s.split(' '))
+        self.lexicon = list(sorted(words))
+        self.radix = len(self.lexicon)
 
-def numbersystem(s1, s2=''):
-    words = set(s1.split(' '))
-    words.update(set(s2.split(' ')))
-    return list(sorted(words))
+    def int(self, s):
+        """ Returns an int based on an input string and a list of words
+            representing a number system.
+        """
+        return sum([self.lexicon.index(w) * (self.radix ** i) for i,w in enumerate(s.split(' '))])
 
+    def lex(self, i):
+        """ inverse of above """
+        output = []
+        remainder = i
+        while remainder > 0:
+            w = remainder % self.radix
+            remainder = remainder // self.radix
+            output.append(self.lexicon[w])
+        return ' '.join(output)
 
-def words2int(s, words):
-    """ Returns an int based on an input string and a list of words
-        representing a number system.
-    """
-    radix = len(words)
-    return sum([words.index(w) * (radix ** i) for i,w in enumerate(s.split(' '))])
-
-
-def int2words(i, words):
-    """ inverse of above """
-    radix = len(words)
-    output = []
-    remainder = i
-    while remainder > 0:
-        w = remainder % radix
-        remainder = remainder // radix
-        #output.append("%d:%s" % (w, words[w]))
-        output.append(words[w])
-    return ' '.join(output)
 
 if __name__ == '__main__':
 
     target = DOUBLE if sys.argv[1] == 'double' else HALF
     with open(target['source'], 'r') as f:
         text = f.read()
-        vocab = numbersystem(text)
-        #print(len(vocab))
-        full = words2int(text, vocab)
-        result = int2words(target['op'](full), vocab)
+        vocab = Lexnum(text)
+        full = vocab.int(text)
+        result = vocab.lex(target['op'](full))
         print(result)
